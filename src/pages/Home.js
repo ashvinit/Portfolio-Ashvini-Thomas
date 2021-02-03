26c6db79b32d4c38b11516f70b5ab5082d1f7757
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-03 11:17:55 -0500

    style: added font style to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,23 @@ import React from "react";
 import "./style.css"
 import me from "../assets/images/me.jpg"
 
+
 function Home() {
     return(
-        <div>
-            <h3>
-                Hey there, I'm Ashvini Thomas!
-            </h3>
-            <h4>
-                I am a Full Stack Web Developer <br />
-                with a background in Information Technology and Informatics.
-            </h4>
-            <img className="my-image" src={me} />
+        <div className="row">
+            <div className="text col-6">
+                <h3>
+                    Hey there, I'm Ashvini Thomas!
+                </h3>
+                <h4>
+                    I am a Full Stack Web Developer <br />
+                    with a background in Information Technology and Informatics.
+                </h4>
+            </div>
+            <div className="image col-6">
+                <img className="my-image" src={me} />
+            </div>
         </div>
-
     );
 }
 
