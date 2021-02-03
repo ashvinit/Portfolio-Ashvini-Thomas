c95bf5cb3295486ac200ecd7dc44146919449ab3
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-03 10:13:09 -0500

    style: styling homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./style.css"
+import me from "../assets/images/me.jpg"
 
 function Home() {
     return(
         <div>
-            
+            <h3>
+                Hey there, I'm Ashvini Thomas!
+            </h3>
+            <h4>
+                I am a Full Stack Web Developer <br />
+                with a background in Information Technology and Informatics.
+            </h4>
+            <img className="my-image" src={me} />
         </div>
 
     );
