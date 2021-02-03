9a87b90e0b933a3b4aa49095ea44291c865dbe74
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-02 20:08:25 -0500

    style: changed color of navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,18 +1,20 @@
 import React from "react";
+import "./style.css";
 
 function NavBar(props) {
   return (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Navbar</a>
+  <nav className="navbar navbar-expand-lg navbar-light">
+    <a className="navbar-brand" href="#">
+      <h1>Ashvini Thomas</h1>
+    </a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-        <a className="nav-item nav-link" href="#">Features</a>
-        <a className="nav-item nav-link" href="#">Pricing</a>
-        <a className="nav-item nav-link disabled" href="#">Disabled</a>
+      <div className="navbar-nav ml-auto">
+        <a className="nav-item nav-link" href="#">Portfolio</a>
+        <a className="nav-item nav-link" href="#">Contact</a>
+        <a className="nav-item nav-link" href="#">About Me</a>
       </div>
     </div>
   </nav>
