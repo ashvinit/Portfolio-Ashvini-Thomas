7ba7c2e842d2fa3aec0abdfd848b8da3c385a504
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-08 13:43:24 -0500

    style: added style to navbar component

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./style.css";
+import NavBar from "../components/NavBar";
 
 
 function Portfolio() {
     return(
         <div>
+            <p className="title">ashvini thomas.</p>
+            <NavBar />
+
 
             
         </div>
