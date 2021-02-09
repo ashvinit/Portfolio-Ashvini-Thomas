9a3578afcf03b579881e8d589f3278bb78621601
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-08 21:52:29 -0500

    feat: added projects to portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,6 +3,11 @@ import "./style.css";
 import NavBar from "../components/NavBar";
 import MiniBrowser from "../components/MiniBrowser";
 import project3 from "../assets/images/phone-eats-first.png";
+import fitness from "../assets/images/fitness-tracker.png";
+import burger from "../assets/images/burger.png";
+import userDir from "../assets/images/userDirectory.png";
+import noteTaker from "../assets/images/noteTaker.png";
+import planner from "../assets/images/planner.png";
 
 
 function Portfolio() {
@@ -11,12 +16,17 @@ function Portfolio() {
             <p className="title">ashvini thomas.</p>
             <NavBar />
             <div className="work">
-                <MiniBrowser value="https://phone-eats-first.herokuapp.com/" src={project3}/>
+                <MiniBrowser className="browser" value="https://phone-eats-first.herokuapp.com/" src={project3}/>
+                <MiniBrowser className="browser" value="https://fitness-tracker-at.herokuapp.com/" src={fitness}/>
             </div>
-            
-
-
-            
+            <div className="work">
+                <MiniBrowser className="browser" value="https://burger-at.herokuapp.com/" src={burger}/>
+                <MiniBrowser className="browser" value="https://ashvinit.github.io/userDirectory/" src={userDir}/>
+            </div> 
+            <div className="work">
+                <MiniBrowser className="browser" value="https://note-taker-at.herokuapp.com/" src={noteTaker}/>
+                <MiniBrowser className="browser" value="https://ashvinit.github.io/dayPlanner/" src={planner}/>
+            </div> 
         </div>
 
     );
