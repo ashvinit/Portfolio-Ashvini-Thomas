11f048ee8a43e500cc4f8c5f9f971136779d1ade
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-08 22:41:25 -0500

    feat: added email form

--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import "./emailForm.css";
 
 class EmailForm extends React.Component {
-    render () {
+    render() {
         return (
             <div className="container">
-
+                <form>
+                    {/* name */}
+                    <label for="name">Full Name</label>
+                    <input type="text" id="name" name="fullname" placeholder="Your name.." />
+                    {/* message */}
+                    <label for="message">Message</label>
+                    <textarea id="message" name="message" placeholder="Message" style={{height:"200px"}}></textarea>
+                    {/* Send */}
+                    <input type="submit" value="Send"></input>
+                </form>
             </div>
         )
     }
