ae8075a4d918e79e5c2d6c82d357722660a7a521
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-09 16:50:31 -0500

    feat: installed emailjs-com and created email sending functionality

--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -1,22 +1,75 @@
 import React from "react";
 import "./emailForm.css";
+import * as emailjs from 'emailjs-com';
+
 
 class EmailForm extends React.Component {
+
+    state={
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const { 
+            name, 
+            email, 
+            subject, 
+            message 
+        } = this.state;
+
+        let templateParams = {
+            name: name,
+            email: email,
+            to_name: 'ashvinithomas@gmail.com',
+            subject: subject,
+            message: message
+        }
+
+        emailjs.send(
+            'service_hk2sbyd',
+            'template_gqq38ms',
+            templateParams,
+            'user_sZ6kGvr63lmC5nKplabKd'
+        )
+
+        this.resetForm()
+    }
+
+    resetForm() {
+        this.setState({
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        });
+    }
+
+    handleChange = (param, event) => {
+        this.setState({ [param]: event.target.value })
+    }
+
     render() {
         return (
             <div className="container" style={{width:"600px", height:"700px"}}>
                 <p className="heading">contact me.</p>
-                <form>
+                <form onSubmit={this.handleSubmit.bind(this)}>
                     {/* name */}
                     <label for="name">Full Name:</label> <br/>
-                    <input type="text" id="name" name="fullname" placeholder="Your name.." /> <br/>
+                    <input value={this.state.name} onChange={this.handleChange.bind(this, 'name')} type="text" id="name" name="fullname" placeholder="Your name.." /> <br/>
+                    {/* email */}
+                    <label for="email">Email Address:</label> <br/>
+                    <input value={this.state.email} onChange={this.handleChange.bind(this, 'email')} type="text" id="email" name="email" placeholder="Your email.." /> <br/>
                     {/* subject */}
-                    {/* name */}
                     <label for="subject">Subject</label> <br/>
-                    <input type="text" id="subject" name="subject" placeholder="Subject..." /> <br/>
+                    <input value={this.state.subject} onChange={this.handleChange.bind(this, 'subject')} type="text" id="subject" name="subject" placeholder="Subject..." /> <br/>
                     {/* message */}
                     <label for="message">Message:</label> <br/>
-                    <textarea id="message" name="message" placeholder="Message"></textarea> <br/>
+                    <textarea value={this.state.message} onChange={this.handleChange.bind(this, 'message')} id="message" name="message" placeholder="Message"></textarea> <br/>
                     {/* Send */}
                     <input type="submit" value="Send"></input>
                 </form>
