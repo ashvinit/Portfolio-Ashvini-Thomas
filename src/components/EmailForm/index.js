f30d0a3bc1b67d2428ad597e39062126c66e1858
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-08 22:10:08 -0500

    feat: created emailForm component

---
+++ src/components/EmailForm/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./emailForm.css";
+
+class EmailForm extends React.Component {
+    render () {
+        return (
+            <div className="container">
+
+            </div>
+        )
+    }
+}
