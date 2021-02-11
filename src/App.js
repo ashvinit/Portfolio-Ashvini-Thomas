0d545cc55e2a3eb3b297197a587a9dd5f29700ef
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-10 21:58:07 -0500

    fix: deploy error

--- src/App.js
+++ src/App.js
@@ -6,12 +6,32 @@ import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 
 function App () {
+
+  let hash = this.props.location.hash.replace('#', '');
+if (hash) {
+  let node = ReactDOM.findDOMNode(this.refs[hash]);
+  if (node) {
+    node.scrollIntoView();
+  }
+}
+
   return (
       <HashRouter basename="/">
         <Switch>
-          <Route exact path="/" component={Home} />       
-          <Route exact path="/portfolio" component={Portfolio} /> 
-          <Route exact path="/contact" component={Contact} />
+          <Route 
+            exact path="/" 
+            render={props => <Index {...props} />} 
+            component={Home} 
+          />       
+          <Route 
+            exact path="/portfolio" 
+            render={props => <Portfolio {...props} />}
+            component={Portfolio} 
+            /> 
+          <Route 
+          exact path="/contact" 
+          render={props => <Contact {...props} />}
+          component={Contact} />
           {/* <Route exact path="/aboutme" component={AboutMe} /> */}
         </Switch>
       </HashRouter>
