d8807444175e1b9d6561a31d1e96702747762c65
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-11 10:30:30 -0500

    fix: error

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <Router basename={`/${process.env.PUBLIC_URL}`}><App /></Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
