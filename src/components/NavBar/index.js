609400d3579aefa8a3cc6e04d0aec350d84701cf
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-11 12:58:43 -0500

    fix: navbar routing

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./navbar.css";
 
 function NavBar() {
+
     return(
         <div className="navbar">
-            <a className="pages" href="/">home.</a>
-            <a className="pages" href="/portfolio">portfolio.</a>
-            <a className="pages" href="/contact">contact.</a>
+            <a className="pages" href="https://ashvinit.github.io/portfolio-ashvini-thomas/#/">home.</a>
+            <a className="pages" href="https://ashvinit.github.io/portfolio-ashvini-thomas/#/portfolio">portfolio.</a>
+            <a className="pages" href="https://ashvinit.github.io/portfolio-ashvini-thomas/#/contact">contact.</a>
             {/* <a className="pages" href="/aboutme">about me.</a> */}
         </div>
     )
