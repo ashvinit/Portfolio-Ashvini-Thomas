c4c1d04ecddbc1da783a79104bb87fcc4c961efd
Ashvini Thomas <ashvinithomas@gmail.com>
2021-02-11 09:36:00 -0500

    fix: error

--- src/App.js
+++ src/App.js
@@ -4,22 +4,34 @@ import { BrowserRouter as HashRouter, Route, Switch } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
+import ReactDOM from "react-dom";
 
 function App () {
+
+  let hash = this.props.location.hash.replace('#', '');
+if (hash) {
+  let node = ReactDOM.findDOMNode(this.refs[hash]);
+  if (node) {
+    node.scrollIntoView();
+  }
+}
 
   return (
       <HashRouter basename="/">
         <Switch>
           <Route 
             exact path="/" 
+            render={props => <Home {...props} />} 
             component={Home} 
           />       
           <Route 
             exact path="/portfolio" 
+            render={props => <Portfolio {...props} />}
             component={Portfolio} 
             /> 
           <Route 
           exact path="/contact" 
+          render={props => <Contact {...props} />}
           component={Contact} />
           {/* <Route exact path="/aboutme" component={AboutMe} /> */}
         </Switch>
